4d4e0318aa45c350498b488cf81335af85e4b403
Barry Saaun <panhaocr@gmail.com>
2026-04-05 22:55:17 +1000

    handle draw stroke on canvas

--- backend/internal/room/drawing.go
+++ backend/internal/room/drawing.go
@@ -1,1 +1,21 @@
 package room
+
+import "encoding/json"
+
+func (r *Room) handleDrawStroke(event Event) {
+	if r.Status != StatusInProgress || event.PlayerID == r.Game.DrawerID {
+		return
+	}
+
+	raw, ok := event.Payload.(json.RawMessage)
+	if !ok {
+		return
+	}
+
+	b, _ := json.Marshal(outgoingMessage{
+		Type:    string(EventDrawStroke),
+		Payload: raw,
+	})
+
+	r.BroadcastExceptSender(event.PlayerID, b)
+}
