fb7390e136041cf678b38d9da633f1385642675d
Barry Saaun <panhaocr@gmail.com>
2026-04-13 13:47:17 +1000

    (feat) add static export and deploy workflow

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import "./src/env";
 
 const nextConfig: NextConfig = {
+  output: "export",
   webpack: (config, { isServer }) => {
     config.plugins.push(new MiniCssExtractPlugin());
     return config;
