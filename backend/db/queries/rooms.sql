0e420504a4728742e74afe8ab31f876501189fde
Barry Saaun <panhaocr@gmail.com>
2026-04-23 22:50:54 +1000

    (fkx) ?? forgot to add this sql ??? SON

---
+++ backend/db/queries/rooms.sql
@@ -0,0 +1,24 @@
+-- name: InsertRoom :exec
+INSERT INTO rooms (id, host_id, host_username, host_display_name, visibility, status, max_players)
+VALUES ($1, $2, $3, $4, $5, $6, $7);
+
+-- name: GetRoomByID :one
+SELECT * FROM rooms WHERE id = $1;
+
+-- name: ListPublicRooms :many
+SELECT * FROM rooms
+WHERE visibility = 'public' AND status = 'waiting'
+ORDER BY created_at DESC;
+
+-- name: UpdateRoomStatus :exec
+UPDATE rooms
+SET status      = $2,
+    finished_at = CASE WHEN $2 = 'finished'::room_status THEN now() ELSE finished_at END
+WHERE id = $1;
+
+-- name: InsertRoomPlayer :exec
+INSERT INTO room_players (room_id, player_id, username, display_name, role)
+VALUES ($1, $2, $3, $4, $5);
+
+-- name: DeleteRoomPlayer :exec
+DELETE FROM room_players WHERE room_id = $1 AND player_id = $2;
